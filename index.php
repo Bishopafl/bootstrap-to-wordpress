5b3dfa1ef8cd05fb755550dc6002f7bed5a879dc
Adam Lopez <Adamf04@gmail.com>
2018-10-08 23:01:40 -0400

    starting a bootstrap to wordpress course, let's see what type of fun we can have integrating bootstrap to wordpress...

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, inital-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" type="text/css" href="assets/img/favicon.ico">
+	<title>Bootstrap to WordPress</title>
+
+	<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- ==================================== -->
+	<!-- 			HEADER SECTION			  -->
+	<!-- ==================================== -->
+	<header>
+		
+	</header>
+	<!-- ==================================== -->
+	<!-- 			HERO SECTION			  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 			OPT IN SECTION			  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 		BOOST YOUR INCOME SECTION	  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 		WHO BENEFITS SECTION		  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 		COURSE FEATURES SECTION		  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 		PROJECT FEATURES SECTION      -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 		VIDEO FEATURETTE SECTION      -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 		INSTRUCTOR SECTION			  -->
+	<!-- ==================================== -->
+	<section> 
+	</section>
+	
+	<!-- ==================================== -->
+	<!-- 		TESTIMONIALS SECTION		  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 			SIGN UP  SECTION		  -->
+	<!-- ==================================== -->
+	<section>
+		
+	</section>
+
+	<!-- ==================================== -->
+	<!-- 			FOOTER SECTION			  -->
+	<!-- ==================================== -->
+	<footer>
+		
+	</footer>
+	<!-- ==================================== -->
+	<!-- 			MODAL SECTION			  -->
+	<!-- ==================================== -->
+	<div>
+		
+	</div>
+
+</body>
+</html>
